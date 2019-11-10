165061057d184252f161b83e54f989b1ec186378
Hrenorubka <43463620+Hrenorubka@users.noreply.github.com>
2019-11-10 16:03:21 +0300

    Постарался поправить ошибки

--- samples/sample_stack.cpp
+++ samples/sample_stack.cpp
@@ -4,6 +4,25 @@
 
 using namespace std;
 
+bool zad1(string s)
+{
+	Stack <char> brackets;
+	int i = 0;
+	while (i < s.length())
+	{
+		if ((s[i] == '(') || (s[i] == '{') || (s[i] == '['))
+			brackets.push(s[i]);
+		else if ((s[i] == ')') && (brackets.top() == '(') || (s[i] == '}') && (brackets.top() == '{') || (s[i] == ']') && (brackets.top() == '['))
+			brackets.pop();
+		else if ((s[i] == ')') || (s[i] == '}') || (s[i] == ']'))
+			return false;
+		i++;
+	}
+	if (brackets.empty())
+		return true;
+	return false;
+}
+
 void main()
 {
 	cout << "Input string for first task: \n";
